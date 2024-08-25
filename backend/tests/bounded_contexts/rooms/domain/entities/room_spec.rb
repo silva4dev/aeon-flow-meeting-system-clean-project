55b596e747f9a80d72fa2986e246e12bc94a5e55
silva4dev <web4dev.lucas@outlook.com>
2024-08-25 11:24:55 -0300

    refactor: :sparkles: involving in describe block

--- backend/tests/bounded_contexts/rooms/domain/entities/room_spec.rb
+++ backend/tests/bounded_contexts/rooms/domain/entities/room_spec.rb
@@ -3,15 +3,17 @@
 require_relative '../../../../../src/bounded_contexts/rooms/domain/entities/room'
 
 RSpec.describe Room, type: :entity do
-  it 'creates a new room' do
-    sut = Room.new(
-      name: 'Conference Room',
-      capacity: 10,
-      location: 'Room 4'
-    )
+  describe '#initialize' do
+    it 'creates a new room' do
+      sut = Room.new(
+        name: 'Conference Room',
+        capacity: 10,
+        location: 'Room 4'
+      )
 
-    expect(sut.capacity).to eq(10)
-    expect(sut.name).to eq('Conference Room')
-    expect(sut.location).to eq('Room 4')
+      expect(sut.capacity).to eq(10)
+      expect(sut.name).to eq('Conference Room')
+      expect(sut.location).to eq('Room 4')
+    end
   end
 end

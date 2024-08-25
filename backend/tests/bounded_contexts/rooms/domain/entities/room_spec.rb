f189374e4d31848d2e70603bacc6688e96e7e9bf
silva4dev <web4dev.lucas@outlook.com>
2024-08-25 15:12:36 -0300

    test: :test_tube: add validations to room entity

--- backend/tests/bounded_contexts/rooms/domain/entities/room_spec.rb
+++ backend/tests/bounded_contexts/rooms/domain/entities/room_spec.rb
@@ -4,17 +4,34 @@ require_relative '../../../../../src/bounded_contexts/rooms/domain/entities/room
 
 RSpec.describe Room, type: :entity do
   describe '#initialize' do
-    it 'creates a new room' do
-      sut = Room.new(
-        name: 'Conference Room',
-        capacity: 10,
-        location: 'Room 4'
-      )
+    context 'with valid attributes' do
+      it 'creates a new room' do
+        room = Room.new(name: 'Conference', capacity: 10, location: 'Room 4')
 
-      expect(sut.id).not_to be_nil
-      expect(sut.capacity).to eq(10)
-      expect(sut.name).to eq('Conference Room')
-      expect(sut.location).to eq('Room 4')
+        expect(room.name).to eq('Conference')
+        expect(room.capacity).to eq(10)
+        expect(room.location).to eq('Room 4')
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'raises an error when name is empty' do
+        expect {
+          Room.new(name: '', capacity: 10, location: 'Room 4')
+        }.to raise_error(ArgumentError, /name/)
+      end
+
+      it 'raises an error when capacity is negative' do
+        expect {
+          Room.new(name: 'Conference Room', capacity: -1, location: 'Room 4')
+        }.to raise_error(ArgumentError, /capacity/)
+      end
+
+      it 'raises an error when location is empty' do
+        expect {
+          Room.new(name: 'Conference Room', capacity: 10, location: '')
+        }.to raise_error(ArgumentError, /location/)
+      end
     end
   end
 end

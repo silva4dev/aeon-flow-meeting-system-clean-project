6d35734a963d7f516962b745326271dad2e8473a
silva4dev <web4dev.lucas@outlook.com>
2024-08-25 19:47:38 -0300

    feat: :sparkles: add base value object

---
+++ backend/src/shared_kernel/value_object.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'dry-struct'
+
+class ValueObject < Dry::Struct::Value
+  # A Value Object (Value Object) is a fundamental concept in Domain-Driven Design (DDD) and represents
+  # an object that is defined exclusively by its attributes, that is, its identity is determined by the
+  # value of its properties, and not by a identifier. single.
+end

e92e4c03894382cc7314535ddeca51941ffd8c41
silva4dev <web4dev.lucas@outlook.com>
2024-08-25 15:11:44 -0300

    feat: :sparkles: add validation to room entity

--- backend/src/bounded_contexts/rooms/domain/entities/room.rb
+++ backend/src/bounded_contexts/rooms/domain/entities/room.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require_relative '../validations/room_validation'
 require_relative '../../../../shared_kernel/entity'
 
 class Room < Entity
   attribute :name, Types::String
   attribute :capacity, Types::Integer
   attribute :location, Types::String
+
+  def initialize(attributes)
+    room_validation = RoomValidation.new.call(attributes)
+    raise ArgumentError, room_validation.errors.to_h unless room_validation.success?
+    super(attributes)
+  end
 end

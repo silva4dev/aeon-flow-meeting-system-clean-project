258d17d0229b4733bf5609987802050473d74f36
silva4dev <web4dev.lucas@outlook.com>
2024-08-24 23:13:31 -0300

    test: :test_tube: creates a new room

---
+++ backend/tests/bounded_contexts/rooms/domain/entities/room_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../../../../../src/bounded_contexts/rooms/domain/entities/room'
+
+RSpec.describe Room, type: :entity do
+  it 'creates a new room' do
+    sut = Room.new(name: 'Conference Room', capacity: 10, location: 'Room 4')
+
+    expect(sut.capacity).to eq(10)
+    expect(sut.name).to eq('Conference Room')
+    expect(sut.location).to eq('Room 4')
+  end
+end

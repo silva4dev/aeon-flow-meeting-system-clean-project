d9a75945747cc0e523e7e7efdeb4872ce6955800
silva4dev <web4dev.lucas@outlook.com>
2024-08-26 08:24:34 -0300

    test: :test_tube: add validation to room entity

---
+++ backend/tests/bounded_contexts/rooms/domain/validations/room_validation_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require_relative '../../../../../src/bounded_contexts/rooms/domain/validations/room_validation'
+
+RSpec.describe RoomValidation do
+  subject(:validator) { described_class.new }
+
+  describe 'validations' do
+    context 'when all attributes are valid' do
+      it 'passes validation' do
+        sut = validator.call({
+          name: 'Conference Room A',
+          capacity: 10,
+          location: 'Building 1, Floor 3'
+        })
+        expect(sut).to be_success
+      end
+    end
+
+    context 'when name is missing' do
+      it 'fails validation' do
+        sut = validator.call({
+          name: '',
+          capacity: 10,
+          location: 'Building 1, Floor 3'
+        })
+        expect(sut).to be_failure
+        expect(sut.errors.to_h).to include(:name)
+      end
+    end
+
+    context 'when capacity is less than or equal to 0' do
+      it 'fails validation' do
+        sut = validator.call({
+          name: 'Conference Room B',
+          capacity: -1,
+          location: 'Building 2, Floor 1'
+        })
+        expect(sut).to be_failure
+        expect(sut.errors.to_h).to include(:capacity)
+      end
+    end
+
+    context 'when location is missing' do
+      it 'fails validation' do
+        sut = validator.call({
+          name: 'Conference Room C',
+          capacity: 5,
+          location: ''
+        })
+        expect(sut).to be_failure
+        expect(sut.errors.to_h).to include(:location)
+      end
+    end
+  end
+end

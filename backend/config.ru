3f17f4a20b37b6091fca4f89bbb5473c1d95454c
silva4dev <web4dev.lucas@outlook.com>
2024-08-28 03:33:25 -0300

    chore: :sparkles: remove unnecessaries files

--- backend/config.ru
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './src/configs/server'
-
-run Server.new
--- backend/src/main/server.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'hanami/api'
-
-class Server < Hanami::API
-  get '/' do
-    { message: 'Hello World!' }.to_json
-  end
-end

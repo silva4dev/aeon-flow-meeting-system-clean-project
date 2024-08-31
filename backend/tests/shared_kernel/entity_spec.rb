989a726593906da30d9f57f33b0a31a4f8580055
silva4dev <web4dev.lucas@outlook.com>
2024-08-31 15:26:11 -0300

    test: :test_tube: add entity base

---
+++ backend/tests/shared_kernel/entity_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../../src/shared_kernel/entity'
+
+RSpec.describe Entity do
+  it 'generates a UUID by default if id is not provided' do
+    sut = Entity.new
+
+    expect(sut.id).not_to be_nil
+  end
+
+  it 'uses the provided id if given' do
+    sut = Entity.new(id: '12345')
+
+    expect(sut.id).to eq('12345')
+  end
+end

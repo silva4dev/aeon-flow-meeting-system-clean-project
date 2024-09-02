ea949d6bf02caf136c78543b0de7924f0f7893e6
silva4dev <web4dev.lucas@outlook.com>
2024-09-01 22:01:45 -0300

    feat: :sparkles: add cassandra room repository

---
+++ backend/src/bounded_contexts/rooms/infrastructure/database/cassandra/cassandra_room_repository.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../../../application/repositories/room_repository'
+
+module Rooms
+  module Infrastructure
+    module Database
+      module Cassandra
+        class CassandraRoomRepository < Rooms::Application::Repositories::RoomRepository
+          def add(task)
+            { id: 1, name: 'Conference', capacity: 10, location: 'Room 4' }
+          end
+        end
+      end
+    end
+  end
+end

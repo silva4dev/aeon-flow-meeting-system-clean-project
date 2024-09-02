b8cbe3f73f0a2830cb126ee3f10d189778ae0280
silva4dev <web4dev.lucas@outlook.com>
2024-09-01 22:00:28 -0300

    feat: :sparkles: add app container to inject dependecies

--- backend/src/bounded_contexts/rooms/application/usecases/create_room_usecase.rb
+++ backend/src/bounded_contexts/rooms/application/usecases/create_room_usecase.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require_relative '../../app_container'
 require_relative '../../../../@shared/usecase'
 
 module Rooms
   module Application
     module Usecases
       class CreateRoomUsecase < Usecase
+        include Rooms::AppContainer::Inject[room_repository: "rooms.room_repository"]
+
         def call
-          if true
-            Success({ message: 'Room created successfully' })
+          result = room_repository.add('Instance of Room Entity')
+
+          if result.any?
+            Success({ message: 'Successfully created room' })
           else
             Failure({ message: 'Failed to create room' })
           end

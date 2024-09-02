48d672debd53da11d6c0e5ebf2af3f317e75837d
silva4dev <web4dev.lucas@outlook.com>
2024-09-01 21:52:36 -0300

    feat: :sparkles: add contract to room repository

---
+++ backend/src/bounded_contexts/rooms/application/repositories/room_repository.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Rooms
+  module Application
+    module Repositories
+      class RoomRepository
+        def add(task)
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end

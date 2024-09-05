0ba801fe8416b062cf724c2afa3978f6df1590a5
silva4dev <web4dev.lucas@outlook.com>
2024-09-05 08:34:34 -0300

    feat: :sparkles: change app container

--- backend/src/bounded_contexts/rooms/app_container.rb
+++ backend/src/bounded_contexts/rooms/app_container.rb
@@ -2,13 +2,13 @@
 
 require 'dry-auto_inject'
 require_relative '../../@shared/container'
-require_relative './infrastructure/database/cassandra/cassandra_room_repository'
+require_relative './infrastructure/repositories/cassandra_room_repository'
 
 module Rooms
   class AppContainer < Container
     namespace :rooms do
       register(:room_repository) {
-        Infrastructure::Database::Cassandra::CassandraRoomRepository.new
+        Infrastructure::Repositories::CassandraRoomRepository.new
       }
     end
 

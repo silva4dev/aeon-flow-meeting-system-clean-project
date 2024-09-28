a1c21175ec5b8f9048e1d11c49cf4a14b02dee11
silva4dev <web4dev.lucas@outlook.com>
2024-09-27 21:41:23 -0300

    test: :test_tube: change validation

--- backend/tests/bounded_contexts/shared_domain/domain/validation_spec.rb
+++ backend/tests/bounded_contexts/shared_domain/domain/validation_spec.rb
@@ -4,7 +4,7 @@ require_relative '../../../../src/bounded_contexts/shared_domain/domain/validati
 
 RSpec.describe SharedDomain::Domain::Validation do
   before(:all) do
-    class ValidationFake < SharedDomain::Domain::Validation
+    class CpfValidationFake < SharedDomain::Domain::Validation
       params do
         required(:dummy_param).filled(:string)
       end
@@ -13,21 +13,21 @@ RSpec.describe SharedDomain::Domain::Validation do
 
   context 'when a basic schema is defined' do
     it 'returns errors for missing required parameters' do
-      sut = ValidationFake.new.call({})
+      sut = CpfValidationFake.new.call({})
 
       expect(sut.success?).to be false
       expect(sut.errors[:dummy_param]).to include('is missing')
     end
 
     it 'validates with provided parameters' do
-      sut = ValidationFake.new.call(dummy_param: 'value')
+      sut = CpfValidationFake.new.call(dummy_param: 'value')
 
       expect(sut.success?).to be true
       expect(sut.errors).to be_empty
     end
 
     it 'fails validation for incorrect field types' do
-      sut = ValidationFake.new.call(dummy_param: 123)
+      sut = CpfValidationFake.new.call(dummy_param: 123)
 
       expect(sut.success?).to be false
       expect(sut.errors[:dummy_param]).to include('must be a string')

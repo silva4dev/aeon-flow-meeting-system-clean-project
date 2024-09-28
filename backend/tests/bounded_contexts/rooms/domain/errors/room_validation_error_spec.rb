938c9d419db84b94c52f072d51458f509b8ffc8b
silva4dev <web4dev.lucas@outlook.com>
2024-09-27 21:40:53 -0300

    test: :test_tube: add custom room validation error

---
+++ backend/tests/bounded_contexts/rooms/domain/errors/room_validation_error_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../../../../../src/bounded_contexts/rooms/domain/errors/room_validation_error'
+
+RSpec.describe Rooms::Domain::Errors::RoomValidationError do
+  let(:errors) { { name: ["can't be blank"], capacity: ["must be greater than 0"] } }
+  let(:error_message) { "Room validation failed: #{errors}" }
+  subject(:sut) { described_class.new(errors) }
+
+  describe '#initialize' do
+    it 'sets the correct error message' do
+      expect(sut.message).to eq(error_message)
+    end
+  end
+end

0ca665cb0b61c4f9f67489ef0f84f0a7f7bce53b
silva4dev <web4dev.lucas@outlook.com>
2024-09-27 21:38:52 -0300

    feat: :sparkles: add comments to usecase

--- backend/src/bounded_contexts/shared_domain/application/usecase.rb
+++ backend/src/bounded_contexts/shared_domain/application/usecase.rb
@@ -5,6 +5,9 @@ require 'dry-monads'
 module SharedDomain
   module Application
     class Usecase
+      # Use cases help capture the functional requirements of a system and understand
+      # the desired behavior of the software from the user's perspective.
+
       include Dry::Monads[:result]
 
       def call

9a230478f66d710384d64af2cab6a6c396c9de0e
silva4dev <web4dev.lucas@outlook.com>
2024-09-28 03:33:27 -0300

    feat: :sparkles: change room mapper

--- backend/src/bounded_contexts/rooms/infrastructure/mappers/room_mapper.rb
+++ backend/src/bounded_contexts/rooms/infrastructure/mappers/room_mapper.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
+require_relative '../../../shared_domain/infrastructure/mapper'
+require_relative '../../domain/entities/room_entity'
+
 module Rooms
   module Infrastructure
     module Mappers
-      class RoomMapper
+      class RoomMapper < SharedDomain::Infrastructure::Mapper
+        def self.to_entity(dao)
+          Domain::Entities::Room.new(
+            id: dao[:id],
+            name: dao[:name],
+            capacity: dao[:capacity],
+            location: dao[:location]
+          )
+        end
+
         def self.to_dao(entity)
           {
             id: entity.id,

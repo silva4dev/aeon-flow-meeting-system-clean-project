e8a01bbf2d9b27d02d1e1e7d6510e04c2bb038e9
silva4dev <web4dev.lucas@outlook.com>
2024-10-05 18:02:50 -0300

    feat: :sparkles: change id to optional

--- backend/src/bounded_contexts/shared_domain/domain/entity.rb
+++ backend/src/bounded_contexts/shared_domain/domain/entity.rb
@@ -8,7 +8,7 @@ module SharedDomain
       # Entity is a fundamental concept in Domain-Driven Design (DDD), which refers to
       # an object that has a unique identity and is defined by its properties and behaviors.
 
-      attribute :id, SharedDomain::Domain::Types::String.optional.default { SecureRandom.uuid }
+      attribute :id, SharedDomain::Domain::Types::String.optional.default(nil)
     end
   end
 end

478bb462116215a42f5a36cc48e0f8905debbd8f
silva4dev <web4dev.lucas@outlook.com>
2024-10-05 18:03:08 -0300

    feat: :sparkles: remove methods unnecessaries

--- backend/src/bounded_contexts/shared_domain/domain/value_object.rb
+++ backend/src/bounded_contexts/shared_domain/domain/value_object.rb
@@ -8,14 +8,6 @@ module SharedDomain
       # A Value Object (Value Object) is a fundamental concept in Domain-Driven Design (DDD) and represents
       # an object that is defined exclusively by its attributes, that is, its identity is determined by the
       # value of its properties, and not by a identifier. single.
-
-      def self.new(*args)
-        super.tap { freeze }
-      end
-
-      def ==(other)
-        other.is_a?(self.class) && attributes == other.attributes
-      end
     end
   end
 end

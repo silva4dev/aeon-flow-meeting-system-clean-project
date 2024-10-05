90205322ea152bc7b687ba5c856f79c86f0eebea
silva4dev <web4dev.lucas@outlook.com>
2024-10-05 18:04:11 -0300

    test: :test_tube: remove tests unnecessaries

--- backend/tests/bounded_contexts/shared_domain/domain/entity_spec.rb
+++ backend/tests/bounded_contexts/shared_domain/domain/entity_spec.rb
@@ -3,12 +3,6 @@
 require_relative '../../../../src/bounded_contexts/shared_domain/domain/entity'
 
 RSpec.describe SharedDomain::Domain::Entity do
-  it 'generates a UUID by default if id is not provided' do
-    sut = SharedDomain::Domain::Entity.new
-
-    expect(sut.id).not_to be_nil
-  end
-
   it 'uses the provided id if given' do
     sut = SharedDomain::Domain::Entity.new(id: '12345')
 

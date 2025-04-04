e81f619f9849755e53f0b8c4ecb92ee0b335b8f2
silva4dev <web4dev.lucas@outlook.com>
2025-04-03 22:13:20 -0300

    feat: ✨ add container to reservation

---
+++ backend/src/lib/reservations/app_container.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'dry-auto_inject'
+require_relative '../shared_domain/container'
+
+module Reservations
+  class AppContainer < SharedDomain::Container
+    Inject = Dry::AutoInject(AppContainer)
+  end
+end

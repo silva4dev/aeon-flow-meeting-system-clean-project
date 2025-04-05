1f22a232069b696788c59c98c6e3fd3157d1b187
silva4dev <web4dev.lucas@outlook.com>
2025-04-05 19:07:21 -0300

    feat: ✨ add rom into room repository

--- backend/src/lib/rooms/app_container.rb
+++ backend/src/lib/rooms/app_container.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 require 'dry-auto_inject'
+
 require_relative '../shared_domain/container'
+require_relative '../../config/rom'
 require_relative './infrastructure/repositories/sqlite_room_repository'
 
 module Rooms
   class AppContainer < SharedDomain::Container
     namespace :rooms do
-      register(:room_repository) {
-        Infrastructure::Repositories::SqliteRoomRepository.new
-      }
+      register(:room_repository) { Infrastructure::Repositories::SqliteRoomRepository.new(Config::Rom.new) }
     end
 
     Inject = Dry::AutoInject(AppContainer)

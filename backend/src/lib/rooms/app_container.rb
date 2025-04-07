4b27f3a45e49d5253eb3e4ab344add7280f4097b
silva4dev <web4dev.lucas@outlook.com>
2025-04-07 08:48:26 -0300

    feat: ✨ add rom config dependency

--- backend/src/lib/rooms/app_container.rb
+++ backend/src/lib/rooms/app_container.rb
@@ -4,12 +4,16 @@ require 'dry-auto_inject'
 
 require_relative '../shared_domain/container'
 require_relative '../../config/rom'
-require_relative './infrastructure/repositories/sqlite_room_repository'
+require_relative 'infrastructure/repositories/sqlite_room_repository'
 
 module Rooms
   class AppContainer < SharedDomain::Container
+    namespace :config do
+      register(:rom) { Config::Rom.new }
+    end
+
     namespace :rooms do
-      register(:room_repository) { Infrastructure::Repositories::SqliteRoomRepository.new(Config::Rom.new) }
+      register(:room_repository) { Infrastructure::Repositories::SqliteRoomRepository.new }
     end
 
     Inject = Dry::AutoInject(AppContainer)

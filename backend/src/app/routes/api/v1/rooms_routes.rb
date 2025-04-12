fda4723c55c1a893755d465dd50526bbbdddfe81
silva4dev <web4dev.lucas@outlook.com>
2025-04-12 14:14:05 -0300

    refactor: ✨ add nested module

--- backend/src/app/routes/api/v1/rooms_routes.rb
+++ backend/src/app/routes/api/v1/rooms_routes.rb
@@ -2,37 +2,40 @@
 
 require_relative '../../../../lib/rooms/application/usecases/create_room_usecase'
 
-module App; end
-module App::Routes; end
-module App::Routes::API; end
-module App::Routes::API::V1; end
+module App
+  module Routes
+    module API
+      module V1
+        module RoomsRoutes
+          module_function
 
-module App::Routes::API::V1::RoomsRoutes
-  module_function
+          def register(app)
+            app.scope 'api/v1' do
+              app.post '/rooms' do
+                input = {
+                  name: params[:name],
+                  capacity: params[:capacity],
+                  location: params[:location]
+                }
 
-  def register(app)
-    app.scope 'api/v1' do
-      app.post '/rooms' do
-        input = {
-          name: params[:name],
-          capacity: params[:capacity],
-          location: params[:location]
-        }
+                result = Rooms::Application::UseCases::CreateRoomUseCase.new.call(input)
 
-        result = Rooms::Application::UseCases::CreateRoomUseCase.new.call(input)
-
-        if result.success?
-          room = result.value!
-          status 201
-          json(
-            id: room.id.value,
-            name: room.name,
-            capacity: room.capacity,
-            location: room.location
-          )
-        else
-          status 422
-          json(error: "Fail to create room.")
+                if result.success?
+                  room = result.value!
+                  status 201
+                  json(
+                    id: room.id.value,
+                    name: room.name,
+                    capacity: room.capacity,
+                    location: room.location
+                  )
+                else
+                  status 422
+                  json(error: "Fail to create room.")
+                end
+              end
+            end
+          end
         end
       end
     end

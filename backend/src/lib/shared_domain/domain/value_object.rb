804e8f3edd60eee3fbb8347fbf0bf3c270946066
silva4dev <web4dev.lucas@outlook.com>
2025-04-12 14:09:55 -0300

    feat: ✨ add freeze for value object

--- backend/src/lib/shared_domain/domain/value_object.rb
+++ backend/src/lib/shared_domain/domain/value_object.rb
@@ -4,10 +4,14 @@ require 'dry-struct'
 
 module SharedDomain
   module Domain
-    class ValueObject < Dry::Struct::Value
+    class ValueObject < Dry::Struct
       # A Value Object (Value Object) is a fundamental concept in Domain-Driven Design (DDD) and represents
       # an object that is defined exclusively by its attributes, that is, its identity is determined by the
       # value of its properties, and not by a identifier. single.
+      def initialize(attributes = {})
+        super(attributes)
+        freeze
+      end
     end
   end
 end

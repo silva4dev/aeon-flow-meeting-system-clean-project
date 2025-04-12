d5189eb5df244c9810959142e2e159008ced3f56
silva4dev <web4dev.lucas@outlook.com>
2025-04-12 14:09:22 -0300

    feat: ✨ add cors middleware

---
+++ backend/src/app/middlewares/cors.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module App
+  module Middlewares
+    class Cors
+      def self.call(headers)
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Headers'] = '*'
+        headers['Access-Control-Allow-Methods'] = '*'
+      end
+    end
+  end
+end

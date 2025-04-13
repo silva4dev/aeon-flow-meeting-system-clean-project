a4ebbbe721be8c162bd4374250a0c73faa68d37d
silva4dev <web4dev.lucas@outlook.com>
2025-04-12 21:05:23 -0300

    feat: ✨ add pattern matcher for room usecase

--- backend/src/lib/rooms/application/usecases/create_room_usecase.rb
+++ backend/src/lib/rooms/application/usecases/create_room_usecase.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'dry/monads'
+require 'dry/matcher'
+require 'dry/matcher/result_matcher'
+
 require_relative '../../app_container'
 require_relative '../../../shared_domain/application/usecase'
 require_relative '../../domain/entities/room_entity'
@@ -8,6 +12,9 @@ module Rooms
   module Application
     module UseCases
       class CreateRoomUseCase < SharedDomain::Application::UseCase
+        include Dry::Monads::Result::Mixin
+        include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+
         def initialize
           @room_repository = Rooms::AppContainer.resolve('rooms.room_repository')
         end
@@ -15,13 +22,15 @@ module Rooms
         def call(input_dto = {})
           room = Domain::Entities::Room.new(
             name: input_dto[:name],
-            capacity: input_dto[:capacity],
-            location: input_dto[:location],
+            capacity: input_dto[:capacity].to_i,
+            location: input_dto[:location]
           )
 
           @room_repository.add(room)
 
           Success(room)
+        rescue StandardError => e
+          Failure(e.message)
         end
       end
     end

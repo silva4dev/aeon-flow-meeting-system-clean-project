6ee34bea5e9cee40389bf3b51906a144f49b8374
silva4dev <web4dev.lucas@outlook.com>
2025-04-13 22:55:53 -0300

    feat: ✨ add handling exceptions

--- backend/src/lib/rooms/application/usecases/create_room_usecase.rb
+++ backend/src/lib/rooms/application/usecases/create_room_usecase.rb
@@ -7,6 +7,7 @@ require 'dry/matcher/result_matcher'
 require_relative '../../app_container'
 require_relative '../../../shared_domain/application/usecase'
 require_relative '../../domain/entities/room_entity'
+require_relative '../../domain/errors/room_validation_error'
 
 module Rooms
   module Application
@@ -22,15 +23,24 @@ module Rooms
         def call(input_dto = {})
           room = Domain::Entities::Room.new(
             name: input_dto[:name],
-            capacity: input_dto[:capacity].to_i,
+            capacity: input_dto[:capacity],
             location: input_dto[:location]
           )
 
           @room_repository.add(room)
 
           Success(room)
+        rescue Rooms::Domain::Errors::RoomValidationError => e
+          Failure(
+            status: :unprocessable_entity,
+            errors: e.notification.errors,
+            message: e.notification.error_messages.join(', ').to_s
+          )
         rescue StandardError => e
-          Failure(e.message)
+          Failure(
+            status: :internal_server_error,
+            message: e.message
+          )
         end
       end
     end

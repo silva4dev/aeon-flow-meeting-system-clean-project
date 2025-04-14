25c5a282765c014be0629f8413bf5add05c74e69
silva4dev <web4dev.lucas@outlook.com>
2025-04-13 22:53:11 -0300

    feat: ✨ adjust validation with notification pattern

--- backend/src/lib/rooms/domain/entities/room_entity.rb
+++ backend/src/lib/rooms/domain/entities/room_entity.rb
@@ -9,19 +9,26 @@ module Rooms
   module Domain
     module Entities
       class Room < SharedDomain::Domain::Entity
-        attribute :name, SharedDomain::Domain::Types::String
-        attribute :capacity, SharedDomain::Domain::Types::Integer
-        attribute :location, SharedDomain::Domain::Types::String
+        attribute :name, SharedDomain::Domain::Types::Strict::String
+        attribute :capacity, SharedDomain::Domain::Types::Strict::Integer
+        attribute :location, SharedDomain::Domain::Types::Strict::String
+
+        extend(Module.new do
+          def new(attributes)
+            notification = Rooms::Domain::Validations::RoomValidation.new.validate_attributes(attributes)
+            raise Rooms::Domain::Errors::RoomValidationError, notification if notification.has_errors?
+
+            super
+          end
+        end)
 
         def initialize(attributes)
-          room_validation = Validations::RoomValidation.new.call(attributes)
-          raise Errors::RoomValidationError.new(room_validation.errors.to_h) unless room_validation.success?
           attributes[:id] = if attributes[:id].is_a?(String)
-            SharedDomain::Domain::ValueObjects::UuidValueObject.new(value: attributes[:id])
-          else
-            SharedDomain::Domain::ValueObjects::UuidValueObject.new
-          end
-          super(attributes)
+                              SharedDomain::Domain::ValueObjects::UuidValueObject.new(value: attributes[:id])
+                            else
+                              SharedDomain::Domain::ValueObjects::UuidValueObject.new
+                            end
+          super
         end
       end
     end

d686c59554ccf47361aaf4cf60bc952344d7a986
silva4dev <web4dev.lucas@outlook.com>
2025-04-16 01:39:31 -0300

    feat: ✨ add create room action

---
+++ backend/src/app/actions/create_room_action.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/shared_domain/web/action'
+require_relative '../../lib/rooms/application/use_cases/create_room_use_case'
+
+module App
+  module Actions
+    class CreateRoomAction < SharedDomain::Web::Action
+      def call(params)
+        input_dto = {
+          name: params[:name],
+          capacity: params[:capacity],
+          location: params[:location]
+        }
+
+        Rooms::Application::UseCases::CreateRoomUseCase.new.call(input_dto) do |m|
+          m.success do |room|
+            {
+              status: 201,
+              body: {
+                id: room.id.value,
+                name: room.name,
+                capacity: room.capacity,
+                location: room.location
+              }
+            }
+          end
+
+          m.failure do |failure|
+            case failure[:status]
+            when :unprocessable_entity
+              {
+                status: 422,
+                body: { errors: failure[:errors] }
+              }
+            else
+              {
+                status: 500,
+                body: { error: failure[:message] }
+              }
+            end
+          end
+        end
+      end
+    end
+  end
+end

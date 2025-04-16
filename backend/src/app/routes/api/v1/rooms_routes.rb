3dd9216f7a32e99180ee9a57e2f1b8dd38b3fa73
silva4dev <web4dev.lucas@outlook.com>
2025-04-16 01:39:50 -0300

    feat: ✨ use create room action

--- backend/src/app/routes/api/v1/rooms_routes.rb
+++ backend/src/app/routes/api/v1/rooms_routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../../../lib/rooms/application/use_cases/create_room_use_case'
+require_relative '../../../actions/create_room_action'
 
 module App
   module Routes
@@ -11,34 +11,10 @@ module App
 
           def register(app)
             app.post '/api/v1/rooms' do
-              input_dto = {
-                name: params[:name],
-                capacity: params[:capacity],
-                location: params[:location]
-              }
-
-              Rooms::Application::UseCases::CreateRoomUseCase.new.call(input_dto) do |m|
-                m.success do |room|
-                  status 201
-                  json(
-                    id: room.id.value,
-                    name: room.name,
-                    capacity: room.capacity,
-                    location: room.location
-                  )
-                end
-
-                m.failure do |failure|
-                  case failure[:status]
-                  when :unprocessable_entity
-                    status 422
-                    json(errors: failure[:errors])
-                  else
-                    status 500
-                    json(error: failure[:message])
-                  end
-                end
-              end
+              action = App::Actions::CreateRoomAction.new
+              result = action.call(params)
+              status result[:status]
+              json(result[:body])
             end
           end
         end

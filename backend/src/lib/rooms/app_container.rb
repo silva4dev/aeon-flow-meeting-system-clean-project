faef707283cad579ec4956c6c342979e6d29f025
silva4dev <web4dev.lucas@outlook.com>
2025-04-19 00:44:07 -0300

    fix: ✨ adjust container for room

--- backend/src/lib/rooms/app_container.rb
+++ backend/src/lib/rooms/app_container.rb
@@ -3,17 +3,14 @@
 require 'dry-auto_inject'
 
 require_relative '../shared_domain/container'
-require_relative '../../database/config/rom'
 require_relative 'infrastructure/repositories/sqlite_room_repository'
 
 module Rooms
   class AppContainer < SharedDomain::Container
-    namespace :config do
-      register(:rom) { Database::Config::Rom.new }
-    end
-
     namespace :rooms do
-      register(:room_repository) { Infrastructure::Repositories::SqliteRoomRepository.new }
+      namespace :infrastructure do
+        register(:room_repository) { Rooms::Infrastructure::Repositories::SqliteRoomRepository.new }
+      end
     end
 
     Inject = Dry::AutoInject(AppContainer)

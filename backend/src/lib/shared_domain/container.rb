8bcfc120dcf863aeb99f583c5883b00ade28a80b
silva4dev <web4dev.lucas@outlook.com>
2025-04-19 00:44:27 -0300

    feat: ✨ add unit of work and room for container

--- backend/src/lib/shared_domain/container.rb
+++ backend/src/lib/shared_domain/container.rb
@@ -2,8 +2,16 @@
 
 require 'dry-container'
 
+require_relative '../../database/config/rom'
+require_relative '../shared_domain/infrastructure/unit_of_work'
+
 module SharedDomain
   class Container
     extend Dry::Container::Mixin
+
+    namespace :infrastructure do
+      register(:rom) { Database::Config::Rom.new }
+      register(:unit_of_work) { SharedDomain::Infrastructure::UnitOfWork.new(Database::Config::Rom.new) }
+    end
   end
 end

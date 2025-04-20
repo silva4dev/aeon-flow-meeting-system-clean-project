4091718081c880dfde35017933e9a3c184240210
silva4dev <web4dev.lucas@outlook.com>
2025-04-20 19:38:22 -0300

    feat: ✨ add endpoints for swagger

--- backend/src/app/server.rb
+++ backend/src/app/server.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
+require 'erb'
+
+require_relative 'docs/swagger_docs'
 require_relative 'routes/api/v1/rooms_routes'
 
 module App
   class Server < Hanami::API
+    get '/api-docs/swagger.json' do
+      App::Docs::SwaggerDocs.generate_docs
+    end
+
+    get '/docs' do
+      redirect '/api-docs/'
+    end
+
+    get '/api-docs/' do
+      headers['Content-Type'] = 'text/html'
+      path = File.expand_path('index.html.erb', './src/app/docs')
+      ERB.new(File.read(path)).result(binding)
+    end
+
     App::Routes::API::V1::RoomsRoutes.register(self)
   end
 end

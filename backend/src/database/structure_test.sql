69dfde0c073bc48197b559eb57efc714f4da5134
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2025-04-26 18:05:30 -0300

    feat: ✨ add database for test

---
+++ backend/src/database/structure_test.sql
@@ -0,0 +1,11 @@
+CREATE TABLE schema_migrations (
+  filename varchar(255),
+);
+
+CREATE TABLE rooms (
+  id varchar(255),
+  name varchar(255),
+  capacity INTEGER,
+  location varchar(255),
+);
+

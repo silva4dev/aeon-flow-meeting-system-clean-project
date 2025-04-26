c468832db22a1fd25fa83a042d06e771030cf6b3
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2025-04-26 18:05:44 -0300

    test: 🧪 add sqlite room repository

---
+++ backend/tests/lib/rooms/infrastructure/repositories/sqlite_room_repository_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../../../../src/lib/rooms/infrastructure/repositories/sqlite_room_repository'
+require_relative '../../../../../src/lib/shared_domain/infrastructure/rom'
+require_relative '../../../../../src/lib/rooms/domain/entities/room_entity'
+
+RSpec.describe Rooms::Infrastructure::Repositories::SqliteRoomRepository do
+  let(:rom) { SharedDomain::Infrastructure::Rom.new }
+  let(:repository) { described_class.new }
+
+  before do
+    DB.tables.each { |table| DB.drop_table?(table) }
+
+    allow(Rooms::AppContainer).to receive(:resolve).with('infrastructure.rom').and_return(rom)
+  end
+
+  after do
+    DB.disconnect
+  end
+
+  describe '#add' do
+    it 'persists a new room' do
+      room = Rooms::Domain::Entities::Room.new(
+        name: 'Conference',
+        capacity: 10,
+        location: 'Room 4'
+      )
+
+      expect {
+        repository.add(room)
+      }.to change { rom[:rooms].count }.by(1)
+    end
+  end
+end

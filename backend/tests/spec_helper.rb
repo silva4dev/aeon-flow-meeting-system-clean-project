374294a5f63f8dd18b7b4fd27b72d6686e0d83cb
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2025-04-26 18:04:25 -0300

    fix: ✨ adjust rules for rubocop

--- backend/tests/spec_helper.rb
+++ backend/tests/spec_helper.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
+require 'sequel'
+require 'fileutils'
+
+ENV['ENVIRONMENT'] ||= 'test'
+
+DB_DIR = File.expand_path('../../src/database', __dir__)
+DB_PATH = File.join(DB_DIR, 'database_test.sqlite3')
+
+DB = Sequel.connect("sqlite://#{DB_PATH}")
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
